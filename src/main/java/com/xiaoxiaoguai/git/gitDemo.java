c2a371a76b47362d79e2bbdc402be8d4e8c914b4
小小怪 <981061402@qq.com>
2023-09-13 15:38:37 +0800

    commit master 01

--- src/main/java/com/xiaoxiaoguai/git/gitDemo.java
+++ src/main/java/com/xiaoxiaoguai/git/gitDemo.java
@@ -8,5 +8,6 @@ public class gitDemo {
         System.out.println("版本1");
         System.out.println("版本2");
         System.out.println("hot_fix 版本");
+        System.out.println("002");
     }
 }
