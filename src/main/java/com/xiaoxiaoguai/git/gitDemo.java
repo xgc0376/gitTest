75240392734cec797290713c13e4b7e8a0ec1d5e
小小怪 <981061402@qq.com>
2023-09-13 15:30:54 +0800

    commit hot-fix

--- src/main/java/com/xiaoxiaoguai/git/gitDemo.java
+++ src/main/java/com/xiaoxiaoguai/git/gitDemo.java
@@ -7,5 +7,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("版本1");
         System.out.println("版本2");
+        System.out.println("hot_fix 版本");
     }
 }
